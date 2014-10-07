75c7ee0891a51d34d518eda228b0108561deba61
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-10-07 19:45:21 +0200

    Added the test's and the methods that displays messages.

--- 2-2-digital-vackarklocka/Program.cs
+++ 2-2-digital-vackarklocka/Program.cs
@@ -9,12 +9,6 @@ namespace _2_2_digital_vackarklocka
     class Program
     {
 
-        #region Fields
-
-        string HorizontalLine;
-
-        #endregion
-
         #region Methods
 
         /// <summary>
@@ -23,11 +17,9 @@ namespace _2_2_digital_vackarklocka
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            AlarmClock test = new AlarmClock(2, 3, 23, 59);
 
-            test.TickTock();
+            AlarmClockTest();
 
-            Console.WriteLine(test.ToString());
         }
 
         /// <summary>
@@ -39,7 +31,7 @@ namespace _2_2_digital_vackarklocka
         /// <param name="minutes"></param>
         private static void Run(AlarmClock ac, int minutes)
         {
-
+            
         }
 
         /// <summary>
@@ -48,7 +40,8 @@ namespace _2_2_digital_vackarklocka
         /// <param name="message"></param>
         private static void ViewErrorMessage(string message)
         {
-
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ResetColor();
         }
 
         /// <summary>
@@ -57,7 +50,173 @@ namespace _2_2_digital_vackarklocka
         /// <param name="header"></param>
         private static void ViewTestHeader(string header)
         {
+            Console.ResetColor();
+            Console.WriteLine(header);
+        }
 
+        private static void AlarmClockTest()
+        {
+            AlarmClock TestObject = new AlarmClock();
+
+            Test1(TestObject);
+
+            Test2(TestObject);
+
+            Test3(TestObject);
+
+            Test4(TestObject);
+
+            Test5(TestObject);
+
+            Test6(TestObject);
+
+            Test7(TestObject);
+        }
+
+        private static void Test1(AlarmClock testObject)
+        {
+            testObject = new AlarmClock();
+
+            string testResult = "empty";
+
+            testResult = string.Format(Test.Test1, testObject.ToString() + Environment.NewLine + Strings.HorizontalLine); //.Replace("\\n", Environment.NewLine)
+
+            ViewTestHeader(testResult);
+        }
+
+        private static void Test2(AlarmClock testObject)
+        {
+            testObject = new AlarmClock(9, 42);
+            string testResult = "empty";
+
+            testResult = string.Format(Test.Test2, testObject.ToString() + Environment.NewLine + Strings.HorizontalLine);
+
+            ViewTestHeader(testResult);
+        }
+
+        private static void Test3(AlarmClock testObject)
+        {
+            testObject = new AlarmClock(13, 24, 7, 35);
+            string testResult = "empty";
+
+            testResult = string.Format(Test.Test3, testObject.ToString() + Environment.NewLine + Strings.HorizontalLine);
+
+            ViewTestHeader(testResult);
+        }
+
+        private static void Test4(AlarmClock testObject)
+        {
+            testObject.Hour = 23;
+            testObject.Minute = 58;
+            string testResult = "empty";
+
+            testResult = string.Format(Test.Test4);
+
+            ViewTestHeader(testResult);
+
+            for (int i = 0; i < 13; i++)
+            {
+                testObject.TickTock();
+
+                ViewTestHeader(testObject.ToString());
+            }
+            ViewTestHeader(Strings.HorizontalLine);
+        }
+
+        private static void Test5(AlarmClock testObject)
+        {
+            testObject.Hour = 6;
+            testObject.Minute = 12;
+            testObject.AlarmHour = 6;
+            testObject.AlarmMinute = 15;
+            string testResult = "empty";
+
+            testResult = string.Format(Test.Test5);
+
+            ViewTestHeader(testResult);
+
+            for (int i = 0; i < 6; i++)
+            {
+                bool larm = testObject.TickTock();
+
+                if (larm)
+                {
+                    ViewTestHeader(testObject.ToString() + Strings.Larm);
+                }
+                else
+                {
+                    ViewTestHeader(testObject.ToString());
+                }
+            }
+            ViewTestHeader(Strings.HorizontalLine);
+        }
+
+        private static void Test6(AlarmClock testObject)
+        {
+            ViewTestHeader(Test.Test6);
+
+            try
+            {
+                testObject.Hour = 99;
+            }
+            catch (Exception)
+            {
+                ViewErrorMessage(Test.Error_Hour);
+            }
+
+            try
+            {
+                testObject.Minute = 99;
+            }
+            catch (Exception)
+            {
+                ViewErrorMessage(Test.Error_Minute);
+            }
+
+            try
+            {
+                testObject.AlarmHour = 99;
+            }
+            catch (Exception)
+            {
+                ViewErrorMessage(Test.Error_Hour);
+            }
+
+            try
+            {
+                testObject.AlarmMinute = 99;
+            }
+            catch (Exception)
+            {
+                ViewErrorMessage(Test.Error_Minute);
+            }
+            
+            ViewTestHeader(Strings.HorizontalLine);
+        }
+
+        private static void Test7(AlarmClock testObject)
+        {
+            ViewTestHeader(Test.Test7);
+            
+            try
+            {
+                testObject = new AlarmClock(99, 99, 7, 35);
+            }
+            catch (Exception)
+            {
+                ViewErrorMessage(Test.Error_Hour);
+            }
+
+            try
+            {
+                testObject = new AlarmClock(13, 24, 99, 99);
+            }
+            catch (Exception)
+            {
+                ViewErrorMessage(Test.Error_Minute);
+            }
+
+            ViewTestHeader(Strings.HorizontalLine);
         }
 
         #endregion
